de100151862cc186ccfa65eabdcad57cf79bf32a
Sangavi K <sangavikrishnan27@gmail.com>
2026-03-13 22:29:14 +0530

    Initial commit

---
+++ src/test/java/com/studyroom/booking/StudyRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyroom.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
